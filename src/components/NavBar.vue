ca44d3a7738e35c251432883c29fe7b01400c51d
Gavy31 <gavysingh0194@gmail.com>
2023-02-07 09:52:17 +0530

    Added contact us layout and Navigation Drawer fixed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { ref } from 'vue'
+import { ref } from 'vue'
     import ExtendMenu from './ExtendMenu.vue';
 
     const drawer =ref(false);
@@ -15,26 +15,20 @@
                 {
                     title: 'Blog',
                     value: 'Blog'
-            },{
-            title: 'Initiatives',
-            value:[{
-                    title: 'Unicef',
-                    value: 'Unicef'
-                },
-                {
-                    title: 'Malaysia',
-                    value: 'Malaysia'
-                }]
             }]
-    const Inititatives=[{
+    const Dropdowns=[{
+                title:'Initiatives',
+                value:[{
                     title: 'Unicef',
                     value: 'Unicef'
                 },
                 {
                     title: 'Malaysia',
                     value: 'Malaysia'
-                }]
-    const About=[{
+                }]},{
+                title:'About',
+                value:
+                [{
                 title: 'Partners',
                 value: 'Partners'
                 
@@ -46,27 +40,44 @@
                 {
                     title: 'Contact',
                     value: 'Contact'
-                }]
+                }]}
+            ]
 
 </script>
 
 <template>
-<v-navigation-drawer app v-model="drawer" location="right" class="bg-black dark">
+<v-navigation-drawer v-model="drawer" app class="bg-black" location="right">
         <v-btn icon color="black" @click.stop="drawer = !drawer">
             <v-icon size="large" color="white">
                 mdi-close
             </v-icon>
         </v-btn>
         <v-list>
-        <v-list-item
-          v-for="(item,i) in navbuttons"
-          :key="i"
-          :to="item.value"
-          link
-          >
-          <v-list-item-title>{{item.title}}</v-list-item-title>
-         
-          </v-list-item>
+        
+            <v-list-item v-for="(item,i) in navbuttons" :key="i" :to= item.value>
+            <v-list-item-title> {{ item.title }}</v-list-item-title>
+            </v-list-item>
+        </v-list>
+        
+        <v-list
+          dense
+          nav
+        >
+            <v-list-group
+            v-for="(dps,i) in Dropdowns"
+            :key="i">
+              <template v-slot:activator="{ props }">
+                <v-list-item
+                v-bind="props">
+                {{dps.title}}
+                </v-list-item>
+              </template>
+              <v-list-item
+                v-for="(item,i) in dps.value"
+                :key="i" :to="item.value">
+                {{item.title}}
+                </v-list-item>
+            </v-list-group>
         </v-list>
     </v-navigation-drawer>
 <v-app-bar app color="black" dark>
@@ -85,16 +96,11 @@
         <div class="text-caption mt-n2 font-weight-bold">foundation</div>
         </div>
     <v-spacer></v-spacer>
-        <!-- <v-btn :v-for="(nutton,i) in navbuttons"
-        :key="i">
-        {{ nutton.title }}
-      </v-btn> -->
-        <!-- <v-btn variant="plain" to="/" class="hidden-sm-and-down"> -->
+
         <v-sheet class="bg-black text-white hidden-sm-and-down">
           <v-btn variant="plain" to="/" > Home</v-btn>
           <v-btn  variant="plain" to="Mission" class="hidden-sm-and-down"> Mission</v-btn>
           <ExtendMenu Name="Initiatives" :Exmenu="Inititatives"/>
-         <!-- <v-btn variant="plain" class="hidden-sm-and-down"> About</v-btn> -->
          <ExtendMenu Name="About" :Exmenu="About" />
         <v-btn variant="plain" to="Blog" class="hidden-sm-and-down"> Blog</v-btn>
         <v-btn class="bg-blue-darken-2" rounded="lg" size="small" align=center> Donate</v-btn>
@@ -107,45 +113,4 @@
 </template>
 
 
-// <script>
-// export default {
-//   name: 'App',
-//     components: {
-
-//     },
-//     data: () => ({
-//         drawer: false,
-//         group: null,
-//         items: [{
-//                 title: 'Home',
-//                 value: '/'
-//             },
-//             {
-//                 title: 'Mission',
-//                 value: 'Mission'
-//             },
-
-//             {
-//                 title: 'Initiatives',
-//                 value: 'Inititatives'
-//             },
-//             {
-//                 title: 'About',
-//                 value: 'Inititatives'
-//             },
-//             {
-//                 title: 'Blog',
-//                 value: 'Blog'
-//             },
-//         ],
-//     }),
-
-//     watch: {
-//         group() {
-//             this.drawer = false
-//         },
-//     },
-
-// }
-// </script>
 
--- src/views/AboutContact.vue
+++ src/views/AboutContact.vue
@@ -1,3 +1,66 @@
+<script setup>
+import {ref} from 'vue';
+
+const firstName=ref('');
+const lastName=ref('');
+const email=ref('');
+const message=ref('');
+
+const rules = (firstName) =>{
+    if(firstName) return
+    return "Please Provide us with your name"
+
+}
+</script>
+
 <template>
-    <h1>Contact</h1>
+<v-row class="ma-0 pa-0">
+<v-col cols="12" md="5">
+    <v-card flat>
+    <v-card-text class="text-md-h5 text-h6 text-black ">
+        Want to be part of our noble mission?
+    </v-card-text>
+    <v-card-text class="text-body-2 text-md-body-1 text-black">
+        
+        "Simply fill in the form below and we will reach out to you."
+          
+    </v-card-text>
+    
+    </v-card>
+</v-col>
+<v-col cols=12 md="5">
+    <v-form @submit.prevent>
+      <v-text-field
+        v-model="firstName"
+        :rules=rules
+        label="First name"
+        class="bg-white"
+      ></v-text-field>
+      <v-text-field
+        v-model="lastName"
+        :rules=rules
+        label="Last name"
+        class="bg-white"
+      ></v-text-field>
+      <v-text-field
+        v-model="email"
+        :rules=rules
+        label="E-Mail"
+        class="bg-white"
+      ></v-text-field>
+      <v-text-field
+        v-model="message"
+        :rules=rules
+        label="Message"
+        class="bg-white"
+      ></v-text-field>
+      <v-btn type="submit" block class="mt-2">Submit</v-btn>
+    </v-form>
+</v-col>
+<v-spacer></v-spacer>
+<v-col md="5" cols="12" class="hidden-sm-and-down pa-0">
+<v-img src="@/assets/contactus.png" contain></v-img>
+</v-col>
+</v-row>
+
 </template>
